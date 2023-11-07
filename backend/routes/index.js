3581820409527c13e52603788a4b0be673311e93
shubham <ss691438@gmail.com>
2023-11-07 11:04:49 +0530

    changes in index.ejs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Api' });
 });
 
 
